a8dd733e77e01676b8ed951f7d765e4c78255fd2
abellusc <alexandra.belluscio@icloud.com>
2020-12-08 02:32:32 -0500

    fix: fix imports

--- packages/gemini-redux-utils/src/index.ts
+++ packages/gemini-redux-utils/src/index.ts
@@ -1,3 +1,9 @@
 import stores from './redux/stores';
 import reducers from './redux/reducers';
 import * as actions from './redux/actions';
+
+export default {
+    stores,
+    reducers,
+    actions,
+};
