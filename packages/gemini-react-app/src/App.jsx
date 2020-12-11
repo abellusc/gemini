52bb045cb514e1c99ff606f841a4535efa97fe0e
abellusc <alexandra.belluscio@icloud.com>
2020-12-10 22:25:05 -0500

    feat: react now showing the beginnings of a UI

--- packages/gemini-react-app/src/App.jsx
+++ packages/gemini-react-app/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes as iconClose, faWindowMinimize as iconMinimize, faWindowMaximize as iconMaximize } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App window">
       <div className="controls">
-        <span class="icon icon-cancel"></span>
+        <FontAwesomeIcon className="ctrlIcon" icon={iconClose} />
+        <FontAwesomeIcon className="ctrlIcon" icon={iconMinimize} />
+        <FontAwesomeIcon className="ctrlIcon" icon={iconMaximize} />
       </div>
       <div className="toolbar toolbar-header">
         <h1 className="title">Gemini Desktop</h1>
