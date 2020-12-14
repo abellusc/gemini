25ee6f5154839a453f0e5976c9a0b7fc48392e7e
abellusc <alexandra.belluscio@icloud.com>
2020-12-13 23:29:44 -0500

    fix: add loading spinner to splash screen

--- packages/gemini-react-app/src/components/loading/Loading.jsx
+++ packages/gemini-react-app/src/components/loading/Loading.jsx
@@ -3,20 +3,54 @@ import { connect } from 'react-redux';
 import * as common from '../../common';
 import '../components.scss';
 import './Loading.scss';
+import classnames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 class Loading extends React.Component {
     constructor() {
         super();
         this.state = {
-            start_time: Date.now()
+            start_time: Date.now(),
+            done: false,
+            rotationalDegrees: 0,
         }
+
+        let i = 0;
+        const c = setInterval(() => {
+            if (i++ > 3) {
+                clearInterval(c);
+                this.setState({
+                    done: true
+                });
+            } else {
+                this.setState({});
+            }
+        }, 1000);
+
+        this.rotator = setInterval(() => {
+            this.setState({
+                rotationalDegrees: (this.state.rotationalDegrees + 1) % 360,
+            });
+        }, 1);
+    }
+    componentWillUnmount() {
+
     }
     render() {
         return (
-            <div className="Loading" style={{ opacity: (Date.now() - this.state.start_time) > 3000 ? this.props.opacity : '1' }}>
-                <div className="app-title" style={{ opacity: (Date.now() - this.state.start_time) > 3000 ? this.props.opacity : '1' }}>gemini</div>
-                <div className="app-subtitle" style={{ opacity: (Date.now() - this.state.start_time) > 3000 ? this.props.opacity : '1' }}>decentralized artificial intelligence</div>
-                <div className="app-copyright" style={{ opacity: (Date.now() - this.state.start_time) > 3000 ? this.props.opacity : '1' }}>&copy; 2020 Solstice Project - github.com/solsticeproject</div>
+            <div className={classnames({
+                'Loading': true,
+                'faded': this.state.done,
+            })}>
+                <div className="app-title">gemini</div>
+                <div className="app-subtitle">decentralized artificial intelligence</div>
+                <div className="app-copyright">&copy; 2020 Solstice Project - github.com/solsticeproject</div>
+                <div className="app-spinner">
+                    <FontAwesomeIcon icon={faSpinner} _nocache={Math.random()} className={classnames({
+                        spinning: true,
+                    })} id="spinner" style={{transform: `rotate(${(this.state.rotationalDegrees % 360)}deg)`}} />
+                </div>
             </div>
         )
     }
